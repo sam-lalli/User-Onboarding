3dfbc9daeeed02e6e7d051cf9dc80d7e7eae5c0e
Sam Lalli <2sam.lalli@gmail.com>
2020-09-16 17:03:14 -0600

    finished product

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,10 +1,108 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Form from './Form'
+import schema from './formSchema'
+import axios from 'axios'
+import * as yup from 'yup'
+
+const initialUsers = []
+
+const initialFormErrors = {
+  name: '',
+  email: '',
+  password: '',
+  terms: '',
+}
+
+const initialValues = {
+  name: '',
+  email: '',
+  password: '',
+  terms: null,
+}
+
+ const initialDisabled = null
 
 function App() {
+  // set state
+  const [users, setUsers] = useState(initialUsers)
+  const [formValues, setFormValues] = useState(initialValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
+
+  const postNewUser = newUser=> {
+    axios.post("https://reqres.in/api/users", newUser)
+      .then(res => {
+        console.log(res)
+        setUsers([...users, res.data])
+        setFormValues(initialValues)
+      })
+      .catch(err => {
+        debugger //eslint-disable-line
+        console.log(err)
+      })
+  }
+
+
+  const validate = (name, value) => {
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then(valid => {
+        setFormErrors({...formErrors, [name]: ""})
+      })
+      .catch(err => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0]
+        })
+      })
+    }  
+
+  const change = (name, value) => {
+    validate(name, value)
+    setFormValues({...formValues, [name]: value})
+
+  }
+
+  const submit = () => {
+    const newUser = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      terms: ['Accepted'],
+    }
+    postNewUser(newUser)
+  }
+
+  useEffect(() => {
+    schema.isValid(formValues)
+      .then(valid => {
+        setDisabled(!valid)
+      })
+  }, [formValues])
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Join Our Team Of Highly Trained Professionals That Are Trained At Being Professional</h1>
+      <Form
+      values={formValues}
+      errors={formErrors}
+      disabled={disabled}
+      change={change}
+      submit={submit}
+      />
+
+      {
+        users.map((user, idx) => (
+          <div key={idx}>
+            <h3>Name: {user.name}</h3>
+            <p>Email: {user.email}</p>
+            <p>Password: {user.password.length}</p>
+            <p>Terms: {user.terms}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
---
+++ team-builder/src/Form.js
@@ -0,0 +1,79 @@
+import React from 'react'
+
+
+
+const From = props => {
+    const { values, errors, disabled, change, submit } = props
+
+    const onChange = e => {
+        const { name, value, type, checked } = e.target
+        const valueToUse = type === "checkbox" ? checked : value
+        change(name, valueToUse)
+    }
+
+
+    const onSubmit = e => {
+        e.preventDefault()
+        submit()
+    }
+
+    return(
+        <form className="form-container" onSubmit={onSubmit}>
+            <div className='form'>
+                <h2>Sign Up here</h2>
+                <div className='form inputs'>
+                    <label>Name
+                        <input 
+                        type='text'
+                        name='name'
+                        onChange={onChange}
+                        value={values.name}
+                        maxLength="16"
+                        placeholder="Enter Username"
+                        />
+                    </label>
+                    <label>Email
+                        <input 
+                        type='email'
+                        name='email'
+                        onChange={onChange}
+                        value={values.email}
+                        maxLength="30"
+                        placeholder="Email"
+                        />
+                    </label>
+                    <label>Password
+                        <input 
+                        type='text'
+                        name='password'
+                        onChange={onChange}
+                        value={values.password}
+                        maxLength='16'
+                        />
+                    </label>
+                    <div className="checkbox">
+                        <label>Click here to agree to the terms and conditions
+                            <input 
+                            type='checkbox'
+                            onChange={onChange}
+                            name='terms'
+                            checked={values.terms}
+                            />
+                        </label>
+                    </div>
+                    <button disabled={disabled}>Submit</button>
+                    <div className='errors'>
+                        <div>{errors.name}</div>
+                        <div>{errors.email}</div>
+                        <div>{errors.password}</div>
+                        <div>{errors.terms}</div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+
+
+export default From
